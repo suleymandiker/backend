d1dc05b013c76df77e97ebbc1a681ce79342abb3
Suleyman Diker <suleyman.diker@gmail.com>
2024-05-31 02:09:01 +0300

    updated posts

--- routes/posts.js
+++ routes/posts.js
@@ -44,5 +44,27 @@ router.post('/', async (req, res) => {
   });
 
 
+// PUT (Güncelleme) işlemi için endpoint
+router.put('/:id', async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      if (!post) return res.status(404).json({ message: 'Post not found' });
+  
+      // Gelen verilerle postu güncelleme
+      post.title = req.body.title;
+      post.description = req.body.description;
+      post.date = req.body.date;
+      post.category = req.body.category;
+      post.views = req.body.views;
+      post.comments = req.body.comments;
+  
+      const updatedPost = await post.save();
+      res.json(updatedPost);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
+
+
 
 module.exports = router;
